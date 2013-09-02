f415d6aa58e01976b53d5303a0bd0a89a2e00738
Peter Foerger <peter.foerger@dkd.de>
2013-09-02 20:58:39 +0200

    Fixed Issue #2
    Font Sizes on default seem to break

--- app/templates/app/scripts/main.js
+++ app/templates/app/scripts/main.js
@@ -1,11 +1,13 @@
 require.config({
     paths: {
         gumby: 'libs/gumby.min',
-        jquery: 'libs/jquery-1.9.1.min'
+        jquery: 'libs/jquery-1.9.1.min',
+        plugins: 'plugins',
+        app: 'app'
     }
 });
 
-require(['jquery','plugins', 'gumby', 'app'], function (app) {
+require(['jquery' ,'plugins', 'gumby', 'app'], function (app) {
     'use strict';
     // use app here
 });
